6d35bab517fa1f26b54f20780473be9166d9c824
brickmaker <3150104891@zju.edu.cn>
2019-10-26 14:28:46 +0800

    27 cubes done

--- index.js
+++ index.js
@@ -7,6 +7,36 @@ const CANVAS_SIZE = {
     height: 800,
 }
 
+const FACE_COLORS = {
+    front: 0xff0000,
+    back: 0x00ff00,
+    top: 0x0000ff,
+    bottom: 0xffff00,
+    left: 0xff00ff,
+    right: 0x00ffff,
+    internal: 0xa0a0a0,
+};
+
+const CUBES_INFO = [
+    {
+        pos: { x: -1, y: -1, z: -1 },
+    },
+]
+
+// functions
+
+function generateCubeMaterial(x, y, z) {
+    const cubeMaterials = [
+        new THREE.MeshBasicMaterial({ color: x == 1 ? FACE_COLORS.right : FACE_COLORS.internal, transparent: true, opacity: 1., side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ color: x == -1 ? FACE_COLORS.left : FACE_COLORS.internal, transparent: true, opacity: 1., side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ color: y == 1 ? FACE_COLORS.top : FACE_COLORS.internal, transparent: true, opacity: 1., side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ color: y == -1 ? FACE_COLORS.bottom : FACE_COLORS.internal, transparent: true, opacity: 1., side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ color: z == 1 ? FACE_COLORS.front : FACE_COLORS.internal, transparent: true, opacity: 1., side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ color: z == -1 ? FACE_COLORS.back : FACE_COLORS.internal, transparent: true, opacity: 1., side: THREE.DoubleSide }),
+    ];
+    return cubeMaterials;
+}
+
 // global objects
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, CANVAS_SIZE.width / CANVAS_SIZE.height, 0.1, 1000);
@@ -21,24 +51,23 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // configs
 renderer.setSize(CANVAS_SIZE.width, CANVAS_SIZE.height);
 
-camera.position.x = 2;
-camera.position.y = 1;
-camera.position.z = 3;
+camera.position.x = 3;
+camera.position.y = 4;
+camera.position.z = 5;
 camera.lookAt(0, 0, 0);
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterials = [
-    new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 1., side: THREE.DoubleSide }),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 1., side: THREE.DoubleSide }),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff, transparent: true, opacity: 1., side: THREE.DoubleSide }),
-    new THREE.MeshBasicMaterial({ color: 0xffff00, transparent: true, opacity: 1., side: THREE.DoubleSide }),
-    new THREE.MeshBasicMaterial({ color: 0xff00ff, transparent: true, opacity: 1., side: THREE.DoubleSide }),
-    new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 1., side: THREE.DoubleSide }),
-];
-
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
-
-scene.add(cube);
+for (let x of [-1, 0, 1]) {
+    for (let y of [-1, 0, 1]) {
+        for (let z of [-1, 0, 1]) {
+            const cube = new THREE.Mesh(cubeGeometry, generateCubeMaterial(x, y, z));
+            cube.position.x = x;
+            cube.position.y = y;
+            cube.position.z = z;
+            scene.add(cube);
+        }
+    }
+}
 
 
 // render function
